c990a6be5ed2c14871aa4a784340f686478fe938
itsaiub <itsaiub@gmail.com>
2019-07-25 00:28:25 +0600

    Navbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, handleOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="Resort" />
+          </Link>
+          <button type="button" className="nav-btn">
+            <FaAlignRight
+              className="nav-icon"
+              onClick={() => handleOpen(!isOpen)}
+            />
+          </button>
+        </div>
+        <ul className={isOpen ? "nav-links show-nav" : "nav-links"}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="rooms">Rooms</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
